9191fe6eec13f283ebaec92fa18e04da91907545
Esme <tovardiazjuanaesmeralda@gmail.com>
2023-10-22 20:24:30 -0600

    subir archivos agregados al proyecto

---
+++ sicefa_backend/src/java/com/blackcode/sicefa/rest/RESTInventario.java
@@ -0,0 +1,40 @@
+package com.blackcode.sicefa.rest;
+
+//import com.blackcode.sicefa.core.ControllerInventario;
+import come.blackcode.sicefa.core.ControllerInventario;
+import com.blackcode.model.Inventario;
+import com.google.gson.Gson;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+
+@Path("inventario")
+
+public class RESTInventario {
+
+    @Path("getAll")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+  
+    public Response getALL() {
+        ControllerInventario ci = new ControllerInventario();
+        List<Inventario> inventarios = null;
+        String out = null;
+        Gson gson = new Gson();
+
+        try {
+            inventarios = ci.getAll("");
+            out = gson.toJson(inventarios);
+        } catch (Exception e) {
+            e.printStackTrace();
+            out = "{\"exception\":" + e.toString().replaceAll("\"", "") + "\"}";
+        }
+        return Response.ok(out).build();
+    }
+    
+
+
+}
